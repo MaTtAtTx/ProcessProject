d9d12db49b12803078a706e18d8997e936a18780
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2017-09-07 09:54:27 -0600

    Added a scanner.

--- src/sample/controller/SampleController.java
+++ src/sample/controller/SampleController.java
@@ -1,5 +1,7 @@
 package sample.controller;
 
+import java.util.Scanner;
+
 public class SampleController
 {
 
@@ -7,5 +9,17 @@ public class SampleController
 	{
 		System.out.println("This is a smaple project to show the github and");
 		System.out.println("eclipse process so I develop muscle memory :D");
+	
+		testScanner();
+	}
+	
+
+	private void testScanner()
+	{
+		Scanner firstScanner; //Declares a Scanner object.
+		firstScanner = new Scanner(System.in); //Instanciates a Scanner Object. (for keyboard input)
+		System.out.println("Please type your favorite food");
+		String answer = firstScanner.nextLine(); //Stores the answer typed in the scanner (everything)
+		System.out.println("Oh, you like to eat " + answer); //Displays the user likes to eat "answer"
 	}
 }
--- src/sample/controller/SampleRunner.java
+++ src/sample/controller/SampleRunner.java
@@ -2,7 +2,7 @@ package sample.controller;
 
 public class SampleRunner
 {
-	public static void main(String [] args)
+	public static void main(String[] args)
 	{
 		SampleController myApp = new SampleController();
 		myApp.start();
